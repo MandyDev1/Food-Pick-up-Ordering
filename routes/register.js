9a7631926467b856ece2666726c0201fe401a558
Mandy Liu <manling512@gmail.com>
2023-09-15 15:57:05 -0600

    Add route handler for get register

--- routes/register.js
+++ routes/register.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const registerUser = require('../db/queries/register/register');
 
+// Route for register submission
 router.post('/register', (req, res) => {
   const user = req.body;
 
@@ -9,6 +10,15 @@ router.post('/register', (req, res) => {
   .then(user => res.json(user))
   .catch(err => res.status(500).json({message: 'An error occurred',
 error: err}));
+});
+
+// Route to go to register page
+router.get('/register', (req, res) => {
+  if (req.session.userId) {
+    return res.redirect('/');
+  }
+
+  res.render('register');
 });
 
 module.exports = router;
