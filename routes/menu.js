4501c776c032a8521faff8afb9d7772a6127551e
Mandy Liu <manling512@gmail.com>
2023-09-16 12:42:17 -0600

    Add getUserById function into the GET users main page route

--- routes/menu.js
+++ routes/menu.js
@@ -5,13 +5,14 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const { cookie } = require('express/lib/response');
 const { getAllMenuItems } = require('../db/queries/menu/01_getAllMenuItems');
 const { getItemById } = require('../db/queries/menu/02_getItemById');
 const { editMenuItem } = require('../db/queries/menu/03_editMenuItem');
 const { addMenuItem } = require('../db/queries/menu/04_addMenuItem');
 const { deleteMenuItem } = require('../db/queries/menu/05_deleteMenuItem');
+const { getUserById, getUserByEmail } = require('../db/queries/users/getUser');
 const db = require('../db/connection');
 const path = require('path');
 
@@ -19,17 +20,26 @@ const path = require('path');
 
 // Retrieve a list of menus
 router.get('/', (req, res) => {
-  // Call the function to get all menu items
-  getAllMenuItems()
-  .then((menuItems) => {
-    // Render the view to display the list of menu items
-    // res.render('menu/list', { menuItems }); // Need to create the 'menu/list' view later
-    res.sendFile(path.join(__dirname, '../views/index.html'));
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send(`Internal server error: ${err.message}`);
-  });
+  getUserById(req.session.userId)
+    .then((data) => {
+      const templateVars = { user: data };
+      // Call the function to get all menu items
+      getAllMenuItems()
+        .then((menuItems) => {
+          // Render the view to display the list of menu items
+          // res.render('menu', { menuItems }, templateVars);
+          res.render('menu', templateVars);
+
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).send(`Internal server error: ${err.message}`);
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Internal server error: ${err.message}`);
+    })
 });
 
 // View detailed information about a specific menu item
@@ -38,17 +48,17 @@ router.get('/:id', (req, res) => {
 
   // Call the function to get the menu item by ID
   getItemById(itemId)
-  .then((menuItem) => {
-    if (!menuItem) {
-      res.status(404).send('Menu item not found');
-    } else {
-      res.render('menu/detail', { menuItem }); // Need to create the 'menu/detail' view later
-    }
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send(`Internal server error: ${err.message}`);
-  });
+    .then((menuItem) => {
+      if (!menuItem) {
+        res.status(404).send('Menu item not found');
+      } else {
+        res.render('menu/detail', { menuItem }); // Need to create the 'menu/detail' view later
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Internal server error: ${err.message}`);
+    });
 });
 
 // OWNER ROUTES
@@ -94,17 +104,17 @@ router.post('/:id/delete', (req, res) => {
 
   // Call the function to delete the menu item by ID
   deleteMenuItem(itemId)
-  .then((deletedMenuItem) => {
-    if (!deletedMenuItem) {
-      res.status(404).send('Menu item not found');
-    } else {
-      res.json({ message: 'Menu item deleted successfully', deletedItem: deletedMenuItem.rows[0] });
-    } // On the client side, After successfully deleting a menu item in the route handler, a notification is sent to the client, a popup window showing information about the deleted menu item is displayed
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send(`Internal server error: ${err.message}`);
-  });
+    .then((deletedMenuItem) => {
+      if (!deletedMenuItem) {
+        res.status(404).send('Menu item not found');
+      } else {
+        res.json({ message: 'Menu item deleted successfully', deletedItem: deletedMenuItem.rows[0] });
+      } // On the client side, After successfully deleting a menu item in the route handler, a notification is sent to the client, a popup window showing information about the deleted menu item is displayed
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Internal server error: ${err.message}`);
+    });
 });
 
 
